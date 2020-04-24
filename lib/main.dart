6283eeeed1244efa6d38c9758dbd2f0119b4cf57
Wesley Silva <wesley.sousa59@gmail.com>
2020-04-24 09:23:08 -0300

    complete mi card flutter app of the doguinho

--- lib/main.dart
+++ lib/main.dart
@@ -7,34 +7,72 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Colors.blueGrey,
         body: SafeArea(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Container(
-                color: Colors.red,
-                width: 100.0,
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/dog.jpg'),
               ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Container(
-                    color: Colors.yellow,
-                    width: 100.0,
-                    height: 100.0,
-                  ),  
-                  Container(
-                    color: Colors.green,
-                    width: 100.0,
-                    height: 100.0,
+              Text(
+                'Doguinho',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                    fontFamily: 'Source Sans Pro',
+                    fontSize: 20.0,
+                    color: Colors.blueGrey.shade100,
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.blueGrey.shade100,
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.blueGrey,
                   ),
-                ],
+                  title: Text(
+                    '+44 123 456 768',
+                    style: TextStyle(
+                        color: Colors.blueGrey.shade900,
+                        fontSize: 20.0,
+                        fontFamily: 'Source Sans Pro'),
+                  ),
+                ),
               ),
-              Container(
-                color: Colors.blue,
-                width: 100.0,
-              ),
+              Card(
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      color: Colors.blueGrey,
+                    ),
+                    title: Text(
+                      'doguinho@gmail.com',
+                      style: TextStyle(
+                          color: Colors.blueGrey.shade900,
+                          fontSize: 20.0,
+                          fontFamily: 'Source Sans Pro'),
+                    ),
+                  )),
             ],
           ),
         ),
